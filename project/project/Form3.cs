691f01e1f102c26b4c06ad48f29338e787f84403
Suaad-abduljalil <sabduljalil.mdbf20@iste.edu.tr>
2022-12-02 17:35:07 +0300

    form3

--- project/project/Form3.cs
+++ project/project/Form3.cs
@@ -16,5 +16,33 @@ namespace project
         {
             InitializeComponent();
         }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Form4 foorm4 = new Form4();
+            foorm4.Show();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Form2 foorm2 = new Form2();
+            foorm2.Show();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            Form1 foorm1 = new Form1();
+            foorm1.Show();
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
